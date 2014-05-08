38ab9e50f02beb8e1778065357ef6ea14bc915cb
Arielle <arielleleone@gmail.com>
2014-05-08 18:15:43 -0400

    ...

--- Project_6.java
+++ Project_6.java
@@ -2,6 +2,10 @@
 Arielle Leone
 Project #6
 5/10/14
+Algorithm: 
+1. Program welcomes user and states purpose. Calls the readInput Method twice to set two mapObjects.
+2. Uses BufferedReader to read input from "state_data.txt" and stores input in mapObjects.
+3. Calls method Breadth.
 */
 import java.lang.*;
 import java.util.*;
@@ -15,17 +19,17 @@ class Driver1{
      
      Node[] mapObject1=readInput();
      Node[] mapObject2=readInput();
-     Map depthObject = new Map(mapObject1);
-     Map breadthObject = new Map(mapObject2);
+     Map breadthObject=new Map(mapObject1);
+     Map depthObject=new Map(mapObject2);
      
-     System.out.println("This program will now list the states using depth first.");
-     System.out.println("               ~~Depth First Results~~");
-     depthObject.Depth(mapObject1[0]);
-     System.out.println("");
      System.out.println("This program will now list the states using breadth first.");
      System.out.println("              ~~Breadth First Results~~");
-     breadthObject.Breadth(mapObject2[0]);
-   }
+     breadthObject.Breadth(mapObject1[0]);
+     System.out.println("");
+    // System.out.println("This program will now list the states using depth first.");
+    // System.out.println("               ~~Depth First Results~~");
+     //depthObject.Depth(mapObject2[0]);
+  }
  public static Node[] readInput(){
       String data;
       int counter;  
@@ -80,13 +84,18 @@ class Map{
          myQueue.enqueue(a.info);
       
          int counter= myQueue.N;
+         a.lv=0;
+         counter++;
       
-         while(!myQueue.isEmpty()){
+         do{
             int oldData=(Integer)myQueue.dequeue();
             //System.println(oldData); //debugging
             Node myNode=this.graph[oldData-1];
-            myNode=myNode.step;
-            while(myNode.step!=null){
+            int increase=myNode.lv;
+            //System.println(increase); //debugging
+            //myNode=myNode.step;
+           
+            do{
               counter++;
               int next=myNode.info;
               //System.out.println(next);//debugging
@@ -94,16 +103,25 @@ class Map{
               //System.out.println(nextNode.info); //debugging
               
               if(nextNode.traverse!=true){
-                  //counter--;
-                  myQueue.enqueue(nextNode.info);
-                  stateObject.printStates(nextNode.info);
                   nextNode.traverse=true;
+                  counter++;
+                  nextNode.lv=increase+1;
+                  //System.out.println(nextNode.lv);//debugging
+                  myQueue.enqueue(nextNode.info);
+                  
+                  stateObject.printStates(nextNode.info);
+                  System.out.println("level " +nextNode.lv);
+                  System.out.println(""); 
                 }
-                else{
-                 ;}
+              
+               else{
+               ;}
+               
                myNode=myNode.step;
-            }
-        }      
+               
+            }while(myNode.step!=null);
+            
+        }while(!myQueue.isEmpty());      
      }
   }
    public void Depth(Node a){
@@ -121,173 +139,174 @@ class Map{
         for(Node node1=a; node1!=null; node1=node1.step){
       //   System.out.println(node.data); //debugging
         }
-       
         for(Node node=a; node!=null; node=node.step){
           if(a.step!=null){
-                int next=a.step.info;
-                Node b=this.graph[next-1];
+             int next=a.step.info;
+             Node b=this.graph[next-1];
                 
                 //System.out.println(q.data);
                 if(b.traverse!=true){
                   Depth(b);
                 }
-                a=a.step;
+                else{
+                ;}
+             a=a.step;
             }
-            else{
-              ;}
-        }}
-        
+          else{
+            ;}
+        }
+       } 
      }
-public Map(Node[] graph){
+   public Map(Node[] graph){
         this.graph = graph;
-}
+   }
 }
 class States{
     public void printStates(int p){
       switch(p){
             case 1: 
-                System.out.println("Washington");
+                System.out.println("Washington, ");
                 break;
             case 2: 
-                System.out.println("Oregon");
+                System.out.println("Oregon, ");
                 break;
             case 3: 
-                System.out.println("California");
+                System.out.println("California, ");
                 break;
             case 4: 
-                System.out.println("Nevada");
+                System.out.println("Nevada, ");
                 break;
             case 5: 
-                System.out.println("Idaho");
+                System.out.println("Idaho, ");
                 break;
             case 6: 
-                System.out.println("Montana");
+                System.out.println("Montana, ");
                 break;
             case 7: 
-                System.out.println("Utah");
+                System.out.println("Utah, ");
                 break;
             case 8: 
-                System.out.println("Arizona");
+                System.out.println("Arizona, ");
                 break;
             case 9: 
-                System.out.println("Wyoming");
+                System.out.println("Wyoming, ");
                 break;
             case 10: 
-                System.out.println("Colorado");
+                System.out.println("Colorado, ");
                 break;
             case 11: 
-                System.out.println("New Mexico");
+                System.out.println("New Mexico, ");
                 break;
             case 12: 
-                System.out.println("Texas");
+                System.out.println("Texas, ");
                 break;
             case 13: 
-                System.out.println("Oklahoma");
+                System.out.println("Oklahoma, ");
                 break;
             case 14: 
-                System.out.println("Kansas");
+                System.out.println("Kansas, ");
                 break;
             case 15: 
-                System.out.println("Nebraska");
+                System.out.println("Nebraska, ");
                 break;
             case 16: 
-                System.out.println("South Dakota");
+                System.out.println("South Dakota, ");
                 break;
             case 17: 
-                System.out.println("North Dakota");
+                System.out.println("North Dakota, ");
                 break;
             case 18: 
-                System.out.println("Minnesota");
+                System.out.println("Minnesota, ");
                 break;
             case 19: 
-                System.out.println("Iowa");
+                System.out.println("Iowa, ");
                 break;
             case 20: 
-                System.out.println("Missouri");
+                System.out.println("Missouri, ");
                 break;
             case 21: 
-                System.out.println("Arkansas");
+                System.out.println("Arkansas, ");
                 break;
             case 22: 
-                System.out.println("Louisiana");
+                System.out.println("Louisiana, ");
                 break;
             case 23: 
-                System.out.println("Mississippi");
+                System.out.println("Mississippi, ");
                 break;
             case 24: 
-                System.out.println("Alabama");
+                System.out.println("Alabama, ");
                 break;
             case 25: 
-                System.out.println("Tennessee");
+                System.out.println("Tennessee, ");
                 break;
             case 26: 
-                System.out.println("Kentucky");
+                System.out.println("Kentucky, ");
                 break;
             case 27: 
-                System.out.println("Indiana");
+                System.out.println("Indiana, ");
                 break;
             case 28: 
-                System.out.println("Illinois");
+                System.out.println("Illinois, ");
                 break;
             case 29: 
-                System.out.println("Wisconsin");
+                System.out.println("Wisconsin, ");
                 break;
             case 30: 
-                System.out.println("Michigan");
+                System.out.println("Michigan, ");
                 break;
             case 31: 
-                System.out.println("Ohio");
+                System.out.println("Ohio, ");
                 break;
             case 32: 
-                System.out.println("West Virginia");
+                System.out.println("West Virginia, ");
                 break;
             case 33: 
-                System.out.println("Pennsylvania");
+                System.out.println("Pennsylvania, ");
                 break;
             case 34: 
-                System.out.println("New York");
+                System.out.println("New York, ");
                 break;
             case 35: 
-                System.out.println("Vermont");
+                System.out.println("Vermont, ");
                 break;
             case 36: 
-                System.out.println("New Hampshire");
+                System.out.println("New Hampshire, ");
                 break;
             case 37: 
-                System.out.println("Maine");
+                System.out.println("Maine, ");
                 break;
             case 38: 
-                System.out.println("Massachusetts");
+                System.out.println("Massachusetts, ");
                 break;
             case 39: 
-                System.out.println("Rhode Island");
+                System.out.println("Rhode Island, ");
                 break;
             case 40: 
-                System.out.println("Connecticut");
+                System.out.println("Connecticut, ");
                 break;
             case 41: 
-                System.out.println("New Jersey");
+                System.out.println("New Jersey, ");
                 break;
             case 42: 
-                System.out.println("Delaware");
+                System.out.println("Delaware, ");
                 break;
             case 43: 
-                System.out.println("Maryland");
+                System.out.println("Maryland, ");
                 break;
             case 44: 
-                System.out.println("Virginia");
+                System.out.println("Virginia, ");
                 break;
             case 45: 
-                System.out.println("North Carolina");
+                System.out.println("North Carolina, ");
                 break;
             case 46: 
-                System.out.println("South Carolina");
+                System.out.println("South Carolina, ");
                 break;
             case 47: 
-                System.out.println("Georgia");
+                System.out.println("Georgia, ");
                 break;
             case 48: 
-                System.out.println("Florida");
+                System.out.println("Florida, ");
                 break;
       }}}
 class Queue{
@@ -326,16 +345,25 @@ class Node{
     int info;
     Node step;
     boolean traverse;
+    Integer lv;
     
     public Node(){
         info=0;
         step=null;
         traverse=false;
+        lv=null;
     }
-    public Node(int a, Node node, boolean b){  
+     public Node(int a, Node node, boolean b){  
         info=a;
         step=node;
         traverse=b;
+        lv=null;
+    }
+    public Node(int a, Node node, boolean b, Integer c){  
+        info=a;
+        step=node;
+        traverse=b;
+        lv=c;
     }
 }
 class Stack{
